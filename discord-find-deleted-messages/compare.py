f8667f89595276b6fe0c21edd7d010dc73829387
pwny <pwny@fixme>
2020-05-26 13:32:03 +0200

    first working version

--- discord-find-deleted-messages/compare.py
+++ discord-find-deleted-messages/compare.py
@@ -21,12 +21,18 @@ def find_latest_timestamp(data):
 def get_messages_id(data):
     messages = []
     for channel in data['meta']['channels']:
-        for message in data['data'][channel]:
-            usr = data['meta']['users'][data['meta']['userindex'][data['data'][channel][message]['u']]]['name']
-            ts = datetime.datetime.fromtimestamp(data['data'][channel][message]['t']/1000).strftime('%c')
-            messages.append((ts,
+        for message_id in data['data'][channel]:
+            chan = data['meta']['channels'][channel]['name']
+            usr = data['meta']['users'][data['meta']['userindex'][data['data'][channel][message_id]['u']]]['name']
+            date = datetime.datetime.fromtimestamp(data['data'][channel][message_id]['t']/1000).strftime('%c')
+            ts = data['data'][channel][message_id]['t']/1000
+            message = data['data'][channel][message_id].get('m')
+            messages.append((chan,
+                             ts,
+                             date,
                              usr,
-                             message, data['data'][channel][message].get('m')))
+                             message_id,
+                             message))
     return messages
 
 
@@ -42,6 +48,26 @@ def load_json(file):
         print('File ', args[0], " not found.")
         exit()
 
+def print_message(message):
+    channel, timestamp, date, usr, message_id, text = message
+    print(f'#{channel} | {date} <{usr}> {text}')
+
+
+
+def print_data(data):
+    for channel in data['meta']['channels']:
+        print(channel)
+        for message in data['data'][channel]:
+            print(f'{channel}:')
+            u, t, m = (data['data'][channel][message]['u'],
+                       data['data'][channel][message]['t'],
+                       data['data'][channel][message].get('m'))
+
+            ts = datetime.datetime.fromtimestamp(t/1000).strftime('%c')
+            u = data['meta']['users'][data['meta']['userindex'][u]]
+            u = u['name']
+            print(f'[{ts}] <{u}>: {m}')
+
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, "h")
@@ -50,28 +76,29 @@ def main(argv):
     if len(opts) or len(args) < 1:
         usage()
     data = load_json(args[0])
+
     reference = get_messages_id(load_json(args[0]))
     newest = get_messages_id(load_json(args[1]))
-    last_timestamp = max([timestamp for timestamp, _, __, ___ in reference])
-    cleaned_ref = set([(timestamp, usr, msg) for timestamp, usr, message, msg in reference if timestamp < last_timestamp])
-    cleaned_new = set([(timestamp, usr, msg) for timestamp, usr, message, msg in newest])
-    deleted_messages = cleaned_new - cleaned_ref
-    print(len(deleted_messages))
-    for dmsg in deleted_messages:
-        print(dmsg)
+    timestamps = [timestamp for _, timestamp, __, ___, ____, ____ in reference]
+    last_timestamp = timestamps[-1]
+    first_timestamp = timestamps[0]
+    last_date = [date for _, timestamp, date, ___, ____, ____ in reference][-1]
+    print(f'last timestamp: {last_timestamp} {last_date}')
+
+    cleaned_new = []
+    for chan, timestamp, date, usr, message_id, msg in newest:
+        if timestamp <= last_timestamp or timestamp > first_timestamp:
+            cleaned_new.append((chan, timestamp, date, usr, message_id, msg))
 
 
-
-def print_data(data):
-    for channel in data['meta']['channels']:
-        print(channel)
-        for message in data['data'][channel]:
-            u, t, m = data['data'][channel][message]['u'], data['data'][channel][message]['t'], data['data'][channel][message].get('m')
-            # ts = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(t)))
-            ts = datetime.datetime.fromtimestamp(t/1000).strftime('%c')
-            u = data['meta']['users'][data['meta']['userindex'][u]]
-            u = u['name']
-            print(f'[{ts}] <{u}>: {m}')
+    messages_id = [message_id for chan, timestamp, date, usr, message_id, msg in cleaned_new]
+    deleted_messages = []
+    for message in reference:
+        # print(f'{message}')
+        if message[4] not in messages_id:
+            print_message(message)
+            deleted_messages.append(message)
+    print('{} deleted messages'.format(len(deleted_messages)))
 
 
 if __name__ == "__main__":
